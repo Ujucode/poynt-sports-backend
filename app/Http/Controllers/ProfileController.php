b7c074a0694fb01c8d36e87cec5daf5c16e730a6
mananabendra <manabendra847@gmail.com>
2023-06-12 19:34:01 +0530

    Commited with login page API, initial commit

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Profile;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+
+class ProfileController extends Controller
+{
+    public function addplayer(Request $request)
+    {
+        $validator = $request->validate([
+            'firstname' => 'required',
+            'lastname' => 'required',
+            'email' => 'required|unique:profiles',
+            'phoneno' => 'bail|required|numeric|digits:10',
+            'dob' => 'required',
+            'gender' => 'bail|required|not_in:default',
+            'password' => 'bail|required|min:6',
+            'cpassword' => 'bail|required|same:password'
+
+        ]);
+
+        $newPlayer = new Profile;
+        $newPlayer->firstname = $request->firstname;
+        $newPlayer->lastname = $request->lastname;
+        $newPlayer->email = $request->email;
+        $newPlayer->phoneno = $request->phoneno;
+        $newPlayer->gender = $request->gender;
+        $newPlayer->dob = $request->dob;
+        $newPlayer->password = Hash::make($request->password);
+        $newPlayer->save();
+        return response()->json($newPlayer->all(), 200);
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+    
+        $profile = Profile::where('email', $credentials['email'])->first();
+    
+        if ($profile && password_verify($credentials['password'], $profile->password)) {
+            return response()->json($profile, 200);
+        } else {
+            return response()->json(['message' => 'Invalid credentials'], 401);
+        }
+    }
+    
+}
